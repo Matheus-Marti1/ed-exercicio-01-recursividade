ab742ccddc6dc4e6e42ca08682adf5dac85c5538
Matheus Marti <0555147+Matheus-Marti1@users.noreply.github.com>
2024-02-21 11:59:03 -0300

    Corrigir texto do input

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		SomaController somaController = new SomaController();
-		int num = Integer.parseInt(JOptionPane.showInputDialog("Insira um número inteiro positivo:"));
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Insira um nÃºmero inteiro positivo:"));
 		int soma = somaController.soma(num);
 		System.out.println(soma);
 	}
